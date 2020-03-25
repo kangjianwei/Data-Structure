30305356f6210287e5f5ddf8695d61a7093edbd1
康建伟 <kangjianwei1990@qq.com>
2020-03-25 10:34:35 +0800

    :recycle: 更新习题2.25的注释，顺便将CFree的代码更新为与其他分支一致

--- CFree/▼配套习题解析/▼02 线性表/▼习题测试文档-02/Question-2.25-main.c
+++ CFree/▼配套习题解析/▼02 线性表/▼习题测试文档-02/Question-2.25-main.c
@@ -1,69 +1,86 @@
 #include <stdio.h>	
 #include "../../../▲课本算法实现/▲02 线性表/01 SequenceList/SequenceList.c"	//**▲02 线性表**//
 
-/* 函数原型 */
-void Algo_2_25(SqList La, SqList Lb, SqList *Lc);
-	
+/*
+ * 题2.25
+ *
+ * 求交集：C=A∩B，要求新链表另辟空间。
+ */
+Status Algo_2_25(SqList La, SqList Lb, SqList* Lc);
+
+// 测试函数，打印元素
 void PrintElem(LElemType_Sq e);
-	//测试函数，打印整型 
-	
-int main(int argc, char *argv[])
-{
-	SqList La, Lb, Lc;
-	int i;
-	
-	if(InitList_Sq(&La) && InitList_Sq(&Lb)) //链表L创建成功
-	{
-		for(i=1; i<=10; i++)
-		{
-			ListInsert_Sq(&La, i, i);
-			ListInsert_Sq(&Lb, i, 2*i);		
-		} 	
-	}
-	
-	printf("La = ");
-	ListTraverse_Sq(La, PrintElem);
-	printf("\n");	
-	printf("Lb = ");
-	ListTraverse_Sq(Lb, PrintElem);
-	printf("\n\n");	
-		
-	InitList_Sq(&Lc); 
-	Algo_2_25(La, Lb, &Lc);
-	printf("Lc = La∩Lb = ");
-	ListTraverse_Sq(Lc, PrintElem); 			//输出L
-	printf("\n\n");
-		
-	return 0;
+
+
+int main(int argc, char* argv[]) {
+    SqList La, Lb, Lc;
+    int i;
+    
+    // 0号单元存储的是数组长度
+    int a[] = {10, 1, 3, 5, 7,  9, 11, 13, 15, 17, 19};
+    int b[] = {8,  1, 5, 7, 8, 10, 15, 17, 20};
+    
+    // 准备测试数据，同一表中的元素值各不相同
+    InitList_Sq(&La);
+    InitList_Sq(&Lb);
+    for(i = 1; i <= a[0]; i++) {
+        ListInsert_Sq(&La, i, a[i]);
+    }
+    for(i = 1; i <= b[0]; i++) {
+        ListInsert_Sq(&Lb, i, b[i]);
+    }
+    printf("La = ");
+    ListTraverse_Sq(La, PrintElem);
+    printf("\n");
+    printf("Lb = ");
+    ListTraverse_Sq(Lb, PrintElem);
+    printf("\n");
+    
+    // 求交集
+    Algo_2_25(La, Lb, &Lc);
+    
+    printf("Lc = ");
+    ListTraverse_Sq(Lc, PrintElem);
+    printf("\n");
+    
+    return 0;
 }
 
-/*━━━━━━━┓
-┃题2.25：C=A∩B┃
-┗━━━━━━━*/
-void Algo_2_25(SqList La, SqList Lb, SqList *Lc)
-{
-	int i, j, k;
-	
-	i = j = 0;
-	k = 1;
-	
-	while(i<La.length && j<Lb.length)
-	{
-		if(La.elem[i]<Lb.elem[j])
-			i++;
-		else if(La.elem[i]>Lb.elem[j])
-			j++;
-		else
-		{
-			ListInsert_Sq(Lc, k, La.elem[i]);			
-			k++;			
-			i++;
-			j++;
-		}
-	}
+// 求交集：C=A∩B，要求新链表另辟空间。
+Status Algo_2_25(SqList La, SqList Lb, SqList* Lc) {
+    int i, j, k;
+    
+    // 初始化Lc
+    InitList_Sq(Lc);
+    
+    // 确保La和Lb存在
+    if(La.elem == NULL || Lb.elem == NULL) {
+        return ERROR;
+    }
+    
+    i = j = 0;  // 遍历La和Lb
+    k = 1;      // 遍历Lc
+    
+    // 只遍历La和Lb的共同部分就行
+    while(i < La.length && j < Lb.length) {
+        if(La.elem[i] < Lb.elem[j]) {
+            i++;
+        } else if(La.elem[i] > Lb.elem[j]) {
+            j++;
+            // 如果La和Lb中的元素相等
+        } else {
+            // 将共同元素插入到Lc
+            ListInsert_Sq(Lc, k, La.elem[i]);
+            k++;
+            i++;
+            j++;
+        }
+    }
+    
+    return OK;
 }
 
-void PrintElem(LElemType_Sq e)
-{
-	printf("%d ", e);
+// 测试函数，打印元素
+void PrintElem(LElemType_Sq e) {
+    printf("%2d ", e);
 }
--- VisualC++/ExerciseBook/02.25/02.25.c
+++ VisualC++/ExerciseBook/02.25/02.25.c
@@ -5,7 +5,7 @@
 /*
  * 题2.25
  *
- * 求交集：C=A∩B。
+ * 求交集：C=A∩B，要求新链表另辟空间。
  */
 Status Algo_2_25(SqList La, SqList Lb, SqList* Lc);
 
@@ -17,20 +17,21 @@ int main(int argc, char* argv[]) {
     SqList La, Lb, Lc;
     int i;
     
-    int a[10] = {1, 2, 2, 3, 4, 4, 9, 9, 10, 12};
-    int b[10] = {1, 1, 2, 2, 3, 3, 4, 5, 12, 13};
+    // 0号单元存储的是数组长度
+    int a[] = {10, 1, 3, 5, 7,  9, 11, 13, 15, 17, 19};
+    int b[] = {8,  1, 5, 7, 8, 10, 15, 17, 20};
     
-    // 准备测试数据
+    // 准备测试数据，同一表中的元素值各不相同
     InitList(&La);
     InitList(&Lb);
-    for(i = 1; i <= 10; i++) {
-        ListInsert(&La, i, a[i - 1]);
-        ListInsert(&Lb, i, b[i - 1]);
+    for(i = 1; i <= a[0]; i++) {
+        ListInsert(&La, i, a[i]);
     }
-    
+    for(i = 1; i <= b[0]; i++) {
+        ListInsert(&Lb, i, b[i]);
+    }
     printf("La = ");
     ListTraverse(La, PrintElem);
-    
     printf("Lb = ");
     ListTraverse(Lb, PrintElem);
     
@@ -43,19 +44,20 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-// 求交集：C=A∩B
+// 求交集：C=A∩B，要求新链表另辟空间。
 Status Algo_2_25(SqList La, SqList Lb, SqList* Lc) {
     int i, j, k;
+    
+    // 初始化Lc
+    InitList(Lc);
     
     // 确保La和Lb存在
     if(La.elem == NULL || Lb.elem == NULL) {
         return ERROR;
     }
     
-    InitList(Lc);
-    
     i = j = 0;  // 遍历La和Lb
-    k = 0;      // 遍历Lc
+    k = 1;      // 遍历Lc
     
     // 只遍历La和Lb的共同部分就行
     while(i < La.length && j < Lb.length) {
@@ -65,8 +67,8 @@ Status Algo_2_25(SqList La, SqList Lb, SqList* Lc) {
             j++;
             // 如果La和Lb中的元素相等
         } else {
-            // Lc中元素可能重复
-            ListInsert(Lc, k+1, La.elem[i]);
+            // 将共同元素插入到Lc
+            ListInsert(Lc, k, La.elem[i]);
             k++;
             i++;
             j++;
@@ -78,5 +80,5 @@ Status Algo_2_25(SqList La, SqList Lb, SqList* Lc) {
 
 // 测试函数，打印元素
 void PrintElem(ElemType e) {
-    printf("%d ", e);
+    printf("%2d ", e);
 }
