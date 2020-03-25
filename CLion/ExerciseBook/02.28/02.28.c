6449fae19f4444460515696e0eadd9ee9b22c573
康建伟 <kangjianwei1990@qq.com>
2020-03-25 12:33:01 +0800

    :bug: (#17)修复习题2.28没有释放无效结点的bug，顺便将CFree的代码更新为与其他分支一致

--- CLion/ExerciseBook/02.28/02.28.c
+++ CLion/ExerciseBook/02.28/02.28.c
@@ -5,9 +5,11 @@
  * 题2.28
  *
  * 求交集：C=A∩B。
- * 不允许C中的元素重复，且C会利用A原有的空间，A被销毁。
+ *
+ * A和B中元素可能重复，但C中元素不重复。
+ * 而且，要求C利用A原有的空间，但是A中的无效结点会被释放。
  */
-LinkList Algo_2_28(LinkList* La, LinkList Lb);
+Status Algo_2_28(LinkList La, LinkList Lb, LinkList* Lc);
 
 // 测试函数，打印元素
 void PrintElem(ElemType e);
@@ -17,15 +19,18 @@ int main(int argc, char* argv[]) {
     LinkList La, Lb, Lc;
     int i;
     
-    int a[10] = {1, 2, 2, 3, 4, 4, 9, 9, 10, 12};
-    int b[10] = {1, 1, 2, 2, 3, 3, 4, 5, 12, 13};
+    // 0号单元存储的是数组长度
+    int a[] = {10, 1, 1, 1, 7, 7,  9, 10, 18, 18, 19};
+    int b[] = {8,  1, 1, 7, 7, 7, 15, 18, 18};
     
-    // 准备测试数据
+    // 准备测试数据，同一表中的元素值可能相同
     InitList(&La);
     InitList(&Lb);
-    for(i = 1; i <= 10; i++) {
-        ListInsert(La, i, a[i - 1]);
-        ListInsert(Lb, i, b[i - 1]);
+    for(i = 1; i <= a[0]; i++) {
+        ListInsert(La, i, a[i]);
+    }
+    for(i = 1; i <= b[0]; i++) {
+        ListInsert(Lb, i, b[i]);
     }
     printf("La = ");
     ListTraverse(La, PrintElem);
@@ -33,7 +38,7 @@ int main(int argc, char* argv[]) {
     ListTraverse(Lb, PrintElem);
     
     // 求交集
-    Lc = Algo_2_28(&La, Lb);
+    Algo_2_28(La, Lb, &Lc);
     
     printf("Lc = ");
     ListTraverse(Lc, PrintElem);
@@ -42,51 +47,72 @@ int main(int argc, char* argv[]) {
 }
 
 
-LinkList Algo_2_28(LinkList* La, LinkList Lb) {
-    LinkList Lc;
+/*
+ * 题2.28
+ *
+ * 求交集：C=A∩B。
+ *
+ * A和B中元素可能重复，但C中元素不重复。
+ * 而且，要求C利用A原有的空间，但是A中的无效结点会被释放。
+ */
+Status Algo_2_28(LinkList La, LinkList Lb, LinkList* Lc) {
     LinkList pa, pb, pc, s;
     
     // 确保La和Lb存在
-    if(La == NULL || *La == NULL || Lb == NULL) {
-        return NULL;
+    if(La == NULL || Lb == NULL) {
+        return ERROR;
     }
     
-    pa = (*La)->next;
+    // Lc利用La的空间
+    *Lc = La;
+    
+    pa = La->next;
     pb = Lb->next;
+    pc = *Lc;
     
-    Lc = *La;
-    Lc->next = NULL;
-    pc = Lc;
-    
-    // 只遍历La和Lb的共同部分就行
+    // 遍历La和Lb的共同部分
     while(pa != NULL && pb != NULL) {
         if(pa->data < pb->data) {
-            s = pa;
-            pa = pa->next;
-            free(s);
+            pc->next = pa->next;
+            free(pa);   // 释放La中的无效结点
+            pa = pc->next;
         } else if(pa->data > pb->data) {
             pb = pb->next;
-        } else {
-            // 确保Lc中的元素不重复
-            if(pc == Lc || pc->data != pa->data) {
-                pc->next = pa;
-                pc = pc->next;
-            }
             
-            pa = pa->next;
+            // 遇到交集元素
+        } else {
+            // 如果Lc不为空，则需要确保Lc中的元素不重复
+            if(pc == (*Lc) || pc->data != pa->data) {
+                pc = pc->next;  // pc游标直接前进，相当于保留了pa指向的元素
+                
+                pa = pa->next;
+            } else {
+                // 如果该交集元素已经存在了，则释放这个结点
+                pc->next = pa->next;
+                free(pa);   // 释放La中的无效结点
+                
+                pa = pc->next;
+            }
+    
+            // pb总是照常前进
             pb = pb->next;
         }
     }
     
+    // Lc已生成，Lc和La共享一段内存
     pc->next = NULL;
+
+    // 释放La中多余的无效结点
+    while(pa!=NULL){
+        s = pa;
+        pa = pa->next;
+        free(s);
+    }
     
-    
-    *La = NULL;
-    
-    return Lc;
+    return OK;
 }
 
 // 测试函数，打印元素
 void PrintElem(ElemType e) {
-    printf("%d ", e);
+    printf("%2d ", e);
 }
